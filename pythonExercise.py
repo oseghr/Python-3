8f84a625de0b6f36f772f457a5f8138ba3ed4e92
Ose Oaikhena <o.oaikh@yahoo.com>
2020-05-01 18:46:57 -0600

    Day54: python exercises

---
+++ pythonExercise.py
@@ -0,0 +1,109 @@
+# Mailing Address
+print("Ose Benson")
+print("453 Ascot Circle SW")
+print("T3H0X3, Calgary \n")
+
+#Hello
+#userName = input("What is your name?\n")
+#print("Hello "+ userName+"!\n")
+
+#Area of a Room
+#print("Enter width:")
+#width = float(input())
+#print("Enter width's unit:")
+#unitWidth = input()
+#print("Enter length:")
+#length = float(input())
+#print("Enter length's unit")
+#unitLength = input()
+
+#if unitWidth == unitLength:
+ #   unit = "square "+unitWidth
+#else:
+#    unit = unitLength+" "+unitWidth
+
+#area = width*length
+#finalArea = str(area)+ " "+unit
+
+#print(finalArea)
+
+#Area of a Field
+#acreX = (float(area)/43560)
+
+#print("There are "+str(acreX)+" square feet in an acre")
+
+#Bottle Deposits
+#numLess = input("How many containers 1 litre or less?\n")
+#numOver = input("How many containers over 1 litre?\n")
+
+#depositLess = float(numLess)*0.10
+#depositOver = float(numOver)*0.25
+
+#refund = format((float(depositLess)+float(depositOver)),".2f")
+
+#print("Your refund is $"+str(refund))
+
+#Tax and Tip
+#mealcost = float(input("Enter cost of meal?\n"))
+
+#taxamount = 0.05*mealcost
+#tipamount = 0.18*mealcost
+
+#print("Meal Cost: "+format(mealcost,".2f"))
+#print("Tax amount: "+format(taxamount,".2f"))
+#print("Tip amount: "+format(tipamount,".2f"))
+
+#print("Grand Total: "+format((mealcost+tipamount+taxamount),".2f"))
+
+#Sum of the First Positive n integers
+#num = int(input("Enter positeve integers \n"))
+
+#sumTotal = (num*(num+1))/2
+#print("The sum total from 1 to "+str(num)+" is "+ str(sumTotal))
+
+
+
+#from selenium import webdriver
+#import pandas as pd
+#from bs4 import BeautifulSoup
+#from time import sleep
+
+#class FindJob():
+#    def __init__(self):
+#        self.driver = webdriver.Chrome()
+
+
+#Widgets and Gizmos
+numWidget = int(input("Number of Widget?\n"))
+numGizmos = int(input("Number of Gizmos?\n"))
+widget = 75*numWidget
+gizmos = 112*numGizmos
+
+totalWeight = widget+gizmos
+
+print("Total Weight: "+str(totalWeight))
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
