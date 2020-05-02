ffeb5ca44a18b74c17527a69bd0ef43d7a8c2580
OO <37249148+oseghr@users.noreply.github.com>
2020-05-01 18:39:44 -0600

    update hello.py
    update hello.py

--- hello.py
+++ hello.py
@@ -1,31 +1,33 @@
 # this program says hello and ask for name
 
-# print("Hello World!")
-#print("What is your name?")
-#myName = input()
-#print("It is good to meet you, " + myName)
-#print("The length of your name is: ")
-#print(len(myName))
-#print("what is your age?")
-#myAge = input()
-#print("You will be " + str(int(myAge) + 1) + " in a year.")
+print("Hello World!")
+print("What is your name?")
 
-#password = "swordfish"
-#if password == "swordfish":
-#    print("Access granted")
-#else:
-#    print("wrong password")
+myName = input()
+print("It is good to meet you, " + myName)
+print("The length of your name is: ")
+print(len(myName))
+print("what is your age?")
 
-#spam = 0
-#while spam < 10:
- #   print("Hello Ose")
- #   print(spam)
- #   spam += 1
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
 
-#spam = 0
-#for num in range(101):
-#    spam += num
-#print(spam)
+password = "swordfish"
+if password == "swordfish":
+    print("Access granted")
+else:
+    print("wrong password")
+
+spam = 0
+while spam < 10:
+    print("Hello Ose")
+    print(spam)
+    spam += 1
+
+spam = 0
+for num in range(101):
+    spam += num
+print(spam)
 
 import sys
 print("Hello")
