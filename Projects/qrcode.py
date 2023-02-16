89c70c35da94fed828a9028908428b497f1a957b
oseghae.oaikhena <oseghae.oaikhena@networkinv.com>
2023-02-16 10:43:43 -0700

    Added QR Code startup function

--- Projects/qrcode.py
+++ Projects/qrcode.py
@@ -2,3 +2,12 @@
 #create a function that collects a text and converts it to a QR code
 #save the QR code as an image
 #run the function
+import qrcode
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4,
+    )
