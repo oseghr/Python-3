ce484b059822cbdf3971b5bb7bf97e467bfcb713
oseghae.oaikhena <oseghae.oaikhena@networkinv.com>
2023-02-17 20:08:15 -0700

    define formulas for interest calculator

--- Projects/interestcalculator.py
+++ Projects/interestcalculator.py
@@ -11,4 +11,12 @@ def main():
     annual_pr = float(input("Enter your annual interest rate:\n"))
     period = int(input("Enter the number of years:\n"))
 
-    
+    #write formulas
+    monthly_int_rate = annual_pr / 1200
+    period_in_months = period * 12
+    monthly_payments = principal * monthly_int_rate /(1 - (1 + monthly_int_rate) ** (-period_in_months))
+
+
+    print(f"Monthly Payment: {monthly_payments}")
+
+main()
