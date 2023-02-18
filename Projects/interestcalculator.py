dc3010bd1f7c17a389514d120f8d45dd2bfb0ef4
oseghae.oaikhena <oseghae.oaikhena@networkinv.com>
2023-02-17 20:27:42 -0700

    completed the interest calculator

--- Projects/interestcalculator.py
+++ Projects/interestcalculator.py
@@ -2,21 +2,33 @@
 #calculate the monthly payments
 #display result
 
+
+print("Interest Loan Calculator")
+print("=========================")
+start_calculator = input("Calculate a loan payment?(Y/N):\n").upper()
+
 def main():
-    print("Interest Loan Calculator")
-    print("=========================")
 
     #define variables
-    principal = float(input("Enter your loan amount:|n"))
+    principal = float(input("Enter your loan amount:\n"))
     annual_pr = float(input("Enter your annual interest rate:\n"))
     period = int(input("Enter the number of years:\n"))
+    
 
     #write formulas
     monthly_int_rate = annual_pr / 1200
     period_in_months = period * 12
     monthly_payments = principal * monthly_int_rate /(1 - (1 + monthly_int_rate) ** (-period_in_months))
 
+    #display monthly payment formatted to 2d.p
+    print(f"Monthly Payment: ${monthly_payments:.2f}\n")
 
-    print(f"Monthly Payment: {monthly_payments}")
+while(start_calculator == "Y"):
+    main()
+    start_calculator = input("Calculate a loan payment?(Y/N):\n").upper()
+    
 
-main()
+
+
+
+
