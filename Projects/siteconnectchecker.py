86e8627ff951896d51152b19045c400ac53cb2c7
Ose Benson <o.oaikhena@yahoo.com>
2023-02-18 18:49:02 -0700

    adding comments to code

--- Projects/siteconnectchecker.py
+++ Projects/siteconnectchecker.py
@@ -1,13 +1,22 @@
-# import urllib
-# import urllib.request to get the data from the url
-# write a function that takes a url
-# returns a response
 
+
+
+
+
+# import urllib
 import urllib.request as urllib
 
 print("This is the site checker connectivity")
+
+#get the url and store in a variable
 site_url = input("Enter your site url to check:\n")
 
-
+# import urllib.request to get the data from the url
+# write a function that takes a url
 def main(url):
     print("Checking connection...")
+
+
+
+
+# returns a response
