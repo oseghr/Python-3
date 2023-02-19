61d5cf9f2147c363037b1ecdec3a021dbbd92f96
Ose Benson <o.oaikhena@yahoo.com>
2023-02-18 19:11:25 -0700

    create a loop to continue checking

--- Projects/siteconnectchecker.py
+++ Projects/siteconnectchecker.py
@@ -3,8 +3,7 @@ import urllib.request as urllib
 
 print("This is the site checker connectivity")
 
-#get the url and store in a variable
-site_url = input("Enter your site url to check:\n")
+checker = input("Want to check a site?(Y/N)\n")
 
 # import urllib.request to get the data from the url
 # write a function that takes a url
@@ -12,11 +11,15 @@ def main(url):
     print("Checking connection...")
     #open the url open
     response = urllib.urlopen(url)
-    
+
     print(f"Connected to {url} successfully!")
     # returns a response
     print(f"The response code was {response.getcode()}")
 
+while(checker == "Y"):
+    #get the url and store in a variable
+    site_url = input("Enter your site url to check:\n")
+    main(site_url)
+    checker = input("Want to check a site?(Y/N)\n")
 
-main(site_url)
 
